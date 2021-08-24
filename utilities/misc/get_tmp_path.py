78224b1f75b2568bef119e483b95768e0bf7afcf
Julian <vilaj46@gmail.com>
2021-08-24 11:54:50 -0400

    Testing tempfile

--- utilities/misc/get_tmp_path.py
+++ utilities/misc/get_tmp_path.py
@@ -4,6 +4,7 @@ import os
 
 def get_tmp_path():
     tf = tempfile.NamedTemporaryFile()
+    print(tf.name)
     filename = tf.name.rindex("\\")
     name = tf.name[filename + 1:len(tf.name)]
     cwd = os.getcwd()
